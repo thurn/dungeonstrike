fc1815011e5c01a9ef4504196da6b5da907013d0
Derek Thurn <derek@fake.email>
2016-09-12 21:57:46 -0700

    movement

---
+++ DungeonStrike/Assets/Source/Movement.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Movement : MonoBehaviour {
+	private List<GGCell> _currentPath;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0)) {
+			var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			var cell = GGGrid.GetCellFromRay(ray, 1000f);
+			var gridObject = GetComponent<GGObject>();
+			Debug.Log(gridObject.Cell);
+			Debug.Log(cell);
+			_currentPath = GGAStar.GetPath(gridObject.Cell, cell, false);
+
+			Debug.Log("Got Path " + _currentPath.Count);
+			foreach (var pathCell in _currentPath) {
+				Debug.Log("Cell " + pathCell);
+			}			
+		}
+	}
+}
