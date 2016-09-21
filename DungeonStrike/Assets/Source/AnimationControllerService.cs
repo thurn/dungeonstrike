1861d2796cd34ba73a15b81b17640db4a481969f
Derek Thurn <derek@fake.email>
2016-09-20 21:08:51 -0700

    initial animation controller service

---
+++ DungeonStrike/Assets/Source/AnimationControllerService.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace DungeonStrike
+{
+    public class AnimationControllerService : MonoBehaviour
+    {
+        public Transform StartingTarget;
+        private Animator _animator;
+        private Vector3 _target;
+
+        public void Start()
+        {
+            _animator = GetComponent<Animator>();
+            SetTarget(StartingTarget.position);
+        }
+
+        public void Update()
+        {
+
+        }
+
+        public void SetTarget(Vector3 target)
+        {
+            _target = target;
+            var rotationQuaternion = Quaternion.LookRotation(_target - transform.position);
+            var rotation = Quaternion.FromToRotation(transform.forward, _target - transform.position);
+            var degrees = rotation.eulerAngles.y;
+            var result = degrees > 180 ? degrees - 360 : degrees;
+            Debug.Log("rotationDegrees: " + result);
+        }
+
+    }
+}
