a4e215dca49e8230c06ae31f1f3bf7e565281af1
Derek Thurn <derek@fake.email>
2016-09-21 22:38:53 -0700

    more movement

--- DungeonStrike/Assets/Source/AnimationControllerService.cs
+++ DungeonStrike/Assets/Source/AnimationControllerService.cs
@@ -15,6 +15,9 @@ namespace DungeonStrike
         private NavMeshAgent _navMeshAgent;
         private Vector2 _smoothDeltaPosition = Vector2.zero;
         private Vector2 _velocity = Vector2.zero;
+        private float _horizontal;
+        private bool _clearAimAngle;
+        private Vector3 _nextPosition = Vector3.zero;
 
         public void Start()
         {
@@ -23,36 +26,76 @@ namespace DungeonStrike
             _navMeshAgent.updatePosition = false;
             _navMeshAgent.updateRotation = false;
             _navMeshAgent.speed = 3.5f;
-            StartCoroutine(SetTargetAfterDelay(StartingTarget.position));
+            _navMeshAgent.SetDestination(StartingTarget.position);
+            //StartCoroutine(SetTargetAfterDelay(StartingTarget.position));
         }
 
         public void Update()
         {
+            if (!_moving) {
+                _moving = true;
+            }
+
+            var angleToNextPosition = AngleToTarget(_navMeshAgent.nextPosition);
+            //Debug.Log("angleToNextPosition: " + angleToNextPosition / 4.0);
+            _animator.SetFloat("HorAimAngle", angleToNextPosition);
+
+            Vector3 worldDeltaPosition = _navMeshAgent.nextPosition - transform.position;
+
+            // Map 'worldDeltaPosition' to local space
+            float dx = Vector3.Dot(transform.right, worldDeltaPosition);
+            float dy = Vector3.Dot(transform.forward, worldDeltaPosition);
+            Vector2 deltaPosition = new Vector2(dx, dy);
+
+            // Low-pass filter the deltaMove
+            float smooth = Mathf.Min(1.0f, Time.deltaTime / 0.15f);
+            _smoothDeltaPosition = Vector2.Lerp(_smoothDeltaPosition, deltaPosition, smooth);
+
+            // Update velocity if time advances
+            if (Time.deltaTime > 1e-5f)
+            {
+                _velocity = _smoothDeltaPosition / Time.deltaTime;
+            }
+
+            bool shouldMove = _velocity.magnitude > 0.5f && _navMeshAgent.remainingDistance > _navMeshAgent.radius;
+
+            // Update animation parameters
+            _animator.SetFloat("InputMagnitude", shouldMove ? 1 : 0);
+            _animator.SetFloat("Horizontal", _velocity.x);
+            _animator.SetFloat("Vertical", _velocity.y);
+        }
+
+        public void Update2()
+        {
+            if (!_navMeshAgent.pathPending)
+            {
+                _nextPosition = _navMeshAgent.nextPosition;
+                Debug.Log("_nextPosition: " + _nextPosition);
+            }
+
+            if (Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                _rotating = true;
+            }
+
+            if (_rotating)
+            {
+                var angle = AngleToTarget(_navMeshAgent.nextPosition);
+                Debug.Log("angle to next position " + angle);
+                return;
+                if (Mathf.Abs(angle) < 1)
+                {
+                    _rotating = false;
+                    _animator.SetFloat("HorAimAngle", 0);
+                }
+                else
+                {
+                    _animator.SetFloat("HorAimAngle", 45);
+                }
+            }
+
             if (_moving)
             {
-                Vector3 worldDeltaPosition = _navMeshAgent.nextPosition - transform.position;
-
-                // Map 'worldDeltaPosition' to local space
-                float dx = Vector3.Dot(transform.right, worldDeltaPosition);
-                float dy = Vector3.Dot(transform.forward, worldDeltaPosition);
-                Vector2 deltaPosition = new Vector2(dx, dy);
-
-                // Low-pass filter the deltaMove
-                float smooth = Mathf.Min(1.0f, Time.deltaTime / 0.15f);
-                _smoothDeltaPosition = Vector2.Lerp(_smoothDeltaPosition, deltaPosition, smooth);
-
-                // Update velocity if time advances
-                if (Time.deltaTime > 1e-5f)
-                {
-                    _velocity = _smoothDeltaPosition / Time.deltaTime;
-                }
-
-                bool shouldMove = _velocity.magnitude > 0.5f && _navMeshAgent.remainingDistance > _navMeshAgent.radius;
-
-                // Update animation parameters
-                _animator.SetFloat("InputMagnitude", shouldMove ? 1 : 0);
-                _animator.SetFloat("Horizontal", _velocity.x);
-                _animator.SetFloat("Vertical", _velocity.y);
             }
         }
 
