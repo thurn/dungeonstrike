e8a8ccb2fcc22660213322e0c8200b1d184f3aa8
Derek Thurn <derek@fake.email>
2016-09-22 23:18:20 -0700

    movement controller walking

---
+++ DungeonStrike/Assets/Source/EmptyInputSource.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using com.ootii.Input;
+using UnityEngine;
+
+namespace DungeonStrike
+{
+    public class EmptyInputSource : MonoBehaviour, IInputSource
+    {
+        float IInputSource.InputFromAvatarAngle
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        float IInputSource.InputFromCameraAngle
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        bool IInputSource.IsEnabled
+        {
+            get
+            {
+                return false;
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        bool IInputSource.IsViewingActivated
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        float IInputSource.MovementSqr
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        float IInputSource.MovementX
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        float IInputSource.MovementY
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        float IInputSource.ViewX
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        float IInputSource.ViewY
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        float IInputSource.GetValue(string rAction)
+        {
+            throw new NotImplementedException();
+        }
+
+        float IInputSource.GetValue(int rKey)
+        {
+            throw new NotImplementedException();
+        }
+
+        bool IInputSource.IsJustPressed(string rAction)
+        {
+            return false;
+        }
+
+        bool IInputSource.IsJustPressed(int rKey)
+        {
+            return false;
+        }
+
+        bool IInputSource.IsJustPressed(KeyCode rKey)
+        {
+            return false;
+        }
+
+        bool IInputSource.IsJustReleased(string rAction)
+        {
+            return false;
+        }
+
+        bool IInputSource.IsJustReleased(int rKey)
+        {
+            return false;
+        }
+
+        bool IInputSource.IsJustReleased(KeyCode rKey)
+        {
+            return false;
+        }
+
+        bool IInputSource.IsPressed(int rKey)
+        {
+            return false;
+        }
+
+        bool IInputSource.IsPressed(string rAction)
+        {
+            return false;
+        }
+
+        bool IInputSource.IsPressed(KeyCode rKey)
+        {
+            return false;
+        }
+
+        bool IInputSource.IsReleased(int rKey)
+        {
+            return false;
+        }
+
+        bool IInputSource.IsReleased(string rAction)
+        {
+            return false;
+        }
+
+        bool IInputSource.IsReleased(KeyCode rKey)
+        {
+            return false;
+        }
+    }
+}
---
+++ DungeonStrike/Assets/Source/MovementAnimator.cs
@@ -0,0 +1,50 @@
+﻿using com.ootii.Actors.AnimationControllers;
+using UnityEngine;
+
+namespace DungeonStrike
+{
+    public class MovementAnimator : MonoBehaviour
+    {
+        public Transform Target;
+        public MotionController MotionController;
+        public GameObject Cube;
+        private NavMeshAgent _navMeshAgent;
+
+        void Start()
+        {
+            _navMeshAgent = GetComponent<NavMeshAgent>();
+            _navMeshAgent.updatePosition = false;
+            _navMeshAgent.updateRotation = false;
+            _navMeshAgent.speed = 1.65f;
+            _navMeshAgent.radius = 2;
+            _navMeshAgent.acceleration = 2;
+            _navMeshAgent.stoppingDistance = 2;
+            _navMeshAgent.angularSpeed = 240;
+        }
+
+        void Update()
+        {
+            if (UnityEngine.Input.GetKeyDown(KeyCode.Space))
+            {
+                _navMeshAgent.SetDestination(Target.position);
+            }
+
+            if (ReachedDestination())
+            {
+                MotionController.ClearTarget();
+            }
+            else
+            {
+                MotionController.SetTargetPosition(_navMeshAgent.nextPosition, 1.0f);
+                Cube.transform.position = _navMeshAgent.nextPosition;
+            }
+        }
+
+        private bool ReachedDestination()
+        {
+            return !_navMeshAgent.pathPending &&
+                _navMeshAgent.remainingDistance <= _navMeshAgent.stoppingDistance &&
+                (!_navMeshAgent.hasPath || _navMeshAgent.velocity.sqrMagnitude == 0f);
+        }
+    }
+}
