4eb19e8fb2581f64d0ceb4a707dd8f4da45c3e4d
Derek Thurn <derek@fake.email>
2016-09-27 09:15:02 -0700

    two-phase working movement

--- DungeonStrike/Assets/Source/MovementAnimator.cs
+++ DungeonStrike/Assets/Source/MovementAnimator.cs
@@ -5,12 +5,15 @@ namespace DungeonStrike
 {
     public class MovementAnimator : MonoBehaviour
     {
-        public Transform Target;
         public MovementStyle MovementStyle;
         private MotionController _motionController;
         private NavMeshAgent _navMeshAgent;
-        private GameObject _indicator;
+        private GameObject _steeringIndicator;
+        private GameObject _nextPositionIndicator;
+        private GameObject _destinationIndicator;
         private bool _moving;
+        private bool _steer;
+        private Vector3? _target;
 
         void Start()
         {
@@ -23,38 +26,62 @@ namespace DungeonStrike
 
         void Update()
         {
-            if (UnityEngine.Input.GetKeyDown(KeyCode.Space))
-            {
-                _moving = true;
-                _navMeshAgent.SetDestination(Target.position);
-            }
-
             if (_moving && ReachedDestination())
             {
                 _motionController.ClearTarget();
                 Debug.Log("ClearTarget");
-                //                _navMeshAgent.Stop();
-                //                _navMeshAgent.ResetPath();
+                _navMeshAgent.ResetPath();
                 _moving = false;
+            }
+            else if (_steer)
+            {
+                _motionController.SetTargetPosition(_navMeshAgent.steeringTarget, 1.0f);
+                _moving = true;
+                _steer = false;
             }
             else if (_moving)
             {
                 _motionController.SetTargetPosition(_navMeshAgent.nextPosition, 1.0f);
             }
 
-            if (_indicator == null)
+            if (_steeringIndicator == null)
             {
-                _indicator = GameObject.CreatePrimitive(PrimitiveType.Cube);
-                _indicator.transform.localScale = new Vector3(0.1f, 1, 0.1f);
-                GameObject.Destroy(_indicator.GetComponent<BoxCollider>());
+                _steeringIndicator = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                _steeringIndicator.transform.localScale = new Vector3(0.1f, 1, 0.1f);
+                GameObject.Destroy(_steeringIndicator.GetComponent<BoxCollider>());
+                _steeringIndicator.GetComponent<Renderer>().material.color = Color.red;
+
+                _nextPositionIndicator = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                _nextPositionIndicator.transform.localScale = new Vector3(0.1f, 1, 0.1f);
+                GameObject.Destroy(_nextPositionIndicator.GetComponent<BoxCollider>());
+                _nextPositionIndicator.GetComponent<Renderer>().material.color = Color.blue;
+
+                _destinationIndicator = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                _destinationIndicator.transform.localScale = new Vector3(0.1f, 1, 0.1f);
+                GameObject.Destroy(_destinationIndicator.GetComponent<BoxCollider>());
+                _destinationIndicator.GetComponent<Renderer>().material.color = Color.green;
             }
 
-            _indicator.transform.position = _navMeshAgent.nextPosition;
+            if (UnityEngine.Input.GetMouseButtonUp(0))
+            {
+                RaycastHit hit;
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                if (Physics.Raycast(ray, out hit))
+                {
+                    _steer = true;
+                    _navMeshAgent.SetDestination(hit.point);
+                    _target = hit.point;
+                }
+            }
+
+            _steeringIndicator.transform.position = _navMeshAgent.steeringTarget;
+            _nextPositionIndicator.transform.position = _navMeshAgent.nextPosition;
+            _destinationIndicator.transform.position = _navMeshAgent.destination;
         }
 
         private bool ReachedDestination()
         {
-            return !_navMeshAgent.pathPending && _navMeshAgent.remainingDistance <= 0.25f;
+            return Vector3.Distance(transform.position, _target.Value) < 0.75f;
         }
     }
 }
