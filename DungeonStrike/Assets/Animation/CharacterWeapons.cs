46579994f5b00cc1cf1645f955e81be850b06074
Derek Thurn <derek@fake.email>
2016-10-08 15:16:11 -0700

    equipping

--- DungeonStrike/Assets/Animation/CharacterWeapons.cs
+++ DungeonStrike/Assets/Animation/CharacterWeapons.cs
@@ -4,17 +4,29 @@ namespace DungeonStrike
 {
     public class CharacterWeapons : MonoBehaviour
     {
-        public Transform RightHandAttachPoint;
+        private const string RightHandAttachPointTag = "RightHandAttachPoint";
+
         public ModelType ModelType;
         public WeaponType WeaponType;
         private Animator _animator;
+        private Transform _rightHandAttachPoint;
 
         void Start()
         {
             _animator = GetComponent<Animator>();
-            WeaponConstants.EquipWeapon(RightHandAttachPoint, WeaponType, ModelType);
+            foreach (var childTransform in GetComponentsInChildren<Transform>())
+            {
+                if (childTransform.tag == RightHandAttachPointTag)
+                {
+                    _rightHandAttachPoint = childTransform;
+                    Debug.Log("Got attach point: " + _rightHandAttachPoint);
+                    break;
+                }
+            }
 
-            // Jump to a random frame to avoid different characters in the idle state
+            WeaponConstants.EquipWeapon(_rightHandAttachPoint, WeaponType, ModelType);
+
+            // Jump to a random frame to prevent different characters in the idle state
             // from synchronizing their movements
             _animator.Play("Idle", 0, UnityEngine.Random.Range(0.0f, 1.0f));
         }
--- DungeonStrike/Assets/Animation/WeaponConstants.cs
+++ DungeonStrike/Assets/Animation/WeaponConstants.cs
@@ -6,83 +6,30 @@ namespace DungeonStrike
     {
         AssaultCharacter,
         Orc,
-        Goblin
+        Goblin,
+        Ogre,
+        Troll
     }
 
     public enum WeaponType
     {
-        Weapon10mmPistol,
-        Weapon12GaugeAutomaticShotgun,
-        Weapon357MGunRevolver,
-        Weapon38Revolver,
-        Weapon44MGunRevolver,
-        Weapon45AutomaticPistol,
-        Weapon50AutomaticPistol,
-        Weapon500MGunRevolver,
-        Weapon9mmAutomaticPistol,
-        Weapon9mmHeavyPistol,
-        WeaponAGUAssaultRifle,
-        WeaponBM4Shotgun,
-        WeaponCowboyRifle,
-        WeaponCzechMachinePistolExtended,
-        WeaponCzechMachinePistol,
-        WeaponFieldRifle,
-        WeaponFrenchAssaultRifle,
-        WeaponKalash,
-        WeaponMG5Submachinegun,
-        WeaponModel10Submachinegun,
-        WeaponModel16AssaultRifle,
-        WeaponModel37Shotgun,
-        WeaponModel500ProtectorShotgun,
-        WeaponModel700Rifle,
-        WeaponModel82HeavySniperRifle,
-        WeaponModel870Shotgun,
-        WeaponSCAssaultRifle,
-        WeaponSDVSniperRifle,
-        WeaponSnubnosed38Revolver,
-        WeaponUZSubmachinegun,
-        WeaponUMGSubmachinegun,
+        AssaultRifle01,
+        AssaultRifle02,
+        AssaultRifle03,
+        AssaultRifle04,
+        AssaultRifle05,
+        AssaultRifle06,
+        AssaultRifle07,
+        AssaultRifle08,
+        AssaultRifle09,
+        AssaultRifle10
     }
 
     public class WeaponConstants
     {
         public static string AssetNameForWeapon(WeaponType weaponType)
         {
-            switch (weaponType)
-            {
-                case WeaponType.Weapon10mmPistol: return "10mm Pistol";
-                case WeaponType.Weapon12GaugeAutomaticShotgun: return "12 Gauge Automatic Shotgun";
-                case WeaponType.Weapon357MGunRevolver: return "357 M-gun Revolver";
-                case WeaponType.Weapon38Revolver: return "38 Revolver";
-                case WeaponType.Weapon44MGunRevolver: return "44 M-gun Revolver";
-                case WeaponType.Weapon45AutomaticPistol: return "45 Automatic Pistol";
-                case WeaponType.Weapon50AutomaticPistol: return "50 Automatic Pistol";
-                case WeaponType.Weapon500MGunRevolver: return "500 M-gun Revolver";
-                case WeaponType.Weapon9mmAutomaticPistol: return "9mm Automatic Pistol";
-                case WeaponType.Weapon9mmHeavyPistol: return "9mm Heavy Pistol";
-                case WeaponType.WeaponAGUAssaultRifle: return "AGU Assault Rifle";
-                case WeaponType.WeaponBM4Shotgun: return "BM4 Shotgun";
-                case WeaponType.WeaponCowboyRifle: return "Cowboy Rifle";
-                case WeaponType.WeaponCzechMachinePistolExtended: return "Czech Machine Pistol Extended";
-                case WeaponType.WeaponCzechMachinePistol: return "Czech Machine Pistol";
-                case WeaponType.WeaponFieldRifle: return "Field Rifle";
-                case WeaponType.WeaponFrenchAssaultRifle: return "French Assault Rifle";
-                case WeaponType.WeaponKalash: return "Kalash";
-                case WeaponType.WeaponMG5Submachinegun: return "MG5 Submachinegun";
-                case WeaponType.WeaponModel10Submachinegun: return "Model 10 Submachinegun";
-                case WeaponType.WeaponModel16AssaultRifle: return "Model 16 Assault Rifle";
-                case WeaponType.WeaponModel37Shotgun: return "Model 37 Shotgun";
-                case WeaponType.WeaponModel500ProtectorShotgun: return "Model 500 Protector Shotgun";
-                case WeaponType.WeaponModel700Rifle: return "Model 700 Rifle";
-                case WeaponType.WeaponModel82HeavySniperRifle: return "Model 82 Heavy Sniper Rifle";
-                case WeaponType.WeaponModel870Shotgun: return "Model 870 Shotgun";
-                case WeaponType.WeaponSCAssaultRifle: return "SC Assault Rifle";
-                case WeaponType.WeaponSDVSniperRifle: return "SDV Sniper Rifle";
-                case WeaponType.WeaponSnubnosed38Revolver: return "Snub-nosed 38 Revolver";
-                case WeaponType.WeaponUZSubmachinegun: return "U-Z Submachinegun";
-                case WeaponType.WeaponUMGSubmachinegun: return "UMG Submachinegun";
-                default: throw new System.SystemException("Unknown weapon type " + weaponType);
-            }
+            return System.Enum.GetName(typeof(WeaponType), weaponType);
         }
 
         public static void EquipWeapon(Transform attachPoint, WeaponType weaponType, ModelType modelType)
@@ -93,52 +40,25 @@ namespace DungeonStrike
                 weapon.transform.SetParent(attachPoint);
                 switch (weaponType)
                 {
-                    case WeaponType.WeaponSCAssaultRifle:
-                        EquipSCAssaultRifle(attachPoint, weapon, modelType);
+                    case WeaponType.AssaultRifle01:
+                        EquipAssaultRifle01(attachPoint, weapon, modelType);
                         break;
-                    case WeaponType.WeaponBM4Shotgun:
-                        EquipBM4Shotgun(attachPoint, weapon, modelType);
-                        break;
-                    case WeaponType.WeaponKalash:
-                        EquipKalash(attachPoint, weapon, modelType);
-                        break;
-                    case WeaponType.WeaponModel82HeavySniperRifle:
-                        EquipM82HeavySniperRifle(attachPoint, weapon, modelType);
-                        break;
-                    case WeaponType.Weapon12GaugeAutomaticShotgun:
-                        Equip12GaugeAutomaticShotgun(attachPoint, weapon, modelType);
-                        break;
-                    case WeaponType.WeaponFrenchAssaultRifle:
-                        EquipFrenchAssaultRifle(attachPoint, weapon, modelType);
+                    case WeaponType.AssaultRifle02:
+                        EquipAssaultRifle02(attachPoint, weapon, modelType);
                         break;
                     default:
                         throw new System.SystemException("Unknown weapon type " + weaponType);
                 }
             });
-
         }
 
-        private static GameObject EquipSCAssaultRifle(Transform attachPoint, GameObject item, ModelType modelType)
-        {
-            switch (modelType)
-            {
-                case ModelType.Goblin:
-                    item.transform.localPosition = new Vector3(-0.27f, 0.1f, 0.05f);
-                    item.transform.localEulerAngles = new Vector3(0, -8, 70);
-                    item.transform.localScale = new Vector3(1f, 1f, 1f);
-                    return item;
-                default:
-                    throw new System.SystemException("Unknown model type " + modelType);
-            }
-        }
-
-        private static GameObject EquipBM4Shotgun(Transform attachPoint, GameObject item, ModelType modelType)
+        private static GameObject EquipAssaultRifle01(Transform attachPoint, GameObject item, ModelType modelType)
         {
             switch (modelType)
             {
                 case ModelType.Orc:
-                    item.transform.localPosition = new Vector3(-0.62f, 0.16f, 0.05f);
-                    item.transform.localEulerAngles = new Vector3(0, -15, 70);
+                    item.transform.localPosition = new Vector3(-0.30f, 0.10f, 0f);
+                    item.transform.localEulerAngles = new Vector3(105, 80, 90);
                     item.transform.localScale = new Vector3(2f, 2f, 2f);
                     return item;
                 default:
@@ -146,62 +66,18 @@ namespace DungeonStrike
             }
         }
 
-        private static GameObject EquipKalash(Transform attachPoint, GameObject item, ModelType modelType)
+        private static GameObject EquipAssaultRifle02(Transform attachPoint, GameObject item, ModelType modelType)
         {
             switch (modelType)
             {
                 case ModelType.AssaultCharacter:
-                    item.transform.localPosition = new Vector3(-0.02f, -0.05f, -0.25f);
-                    item.transform.localEulerAngles = new Vector3(0, -95, 100);
-                    item.transform.localScale = Vector3.one;
+                    item.transform.localPosition = new Vector3(-0.04f, -0.04f, -0.10f);
+                    item.transform.localEulerAngles = new Vector3(87, 0, 90);
+                    item.transform.localScale = new Vector3(1f, 1f, 1f);
                     return item;
                 default:
                     throw new System.SystemException("Unknown model type " + modelType);
             }
         }
-
-        private static GameObject EquipM82HeavySniperRifle(Transform attachPoint, GameObject item, ModelType modelType)
-        {
-            switch (modelType)
-            {
-                case ModelType.AssaultCharacter:
-                    item.transform.localPosition = new Vector3(-0.08f, -0.03f, -0.23f);
-                    item.transform.localEulerAngles = new Vector3(5, -95, 100);
-                    item.transform.localScale = Vector3.one;
-                    return item;
-                default:
-                    throw new System.SystemException("Unknown model type " + modelType);
-            }
-        }
-
-
-        private static GameObject Equip12GaugeAutomaticShotgun(Transform attachPoint, GameObject item, ModelType modelType)
-        {
-            switch (modelType)
-            {
-                case ModelType.AssaultCharacter:
-                    item.transform.localPosition = new Vector3(-0.04f, -0.05f, -0.30f);
-                    item.transform.localEulerAngles = new Vector3(0, -95, 100);
-                    item.transform.localScale = Vector3.one;
-                    return item;
-                default:
-                    throw new System.SystemException("Unknown model type " + modelType);
-            }
-        }
-
-        private static GameObject EquipFrenchAssaultRifle(Transform attachPoint, GameObject item, ModelType modelType)
-        {
-            switch (modelType)
-            {
-                case ModelType.Orc:
-                    item.transform.localPosition = new Vector3(-0.60f, 0.13f, 0.10f);
-                    item.transform.localEulerAngles = new Vector3(0, -10, 75);
-                    item.transform.localScale = new Vector3(2.0f, 2.0f, 2.0f);
-                    return item;
-                default:
-                    throw new System.SystemException("Unknown model type " + modelType);
-            }
-        }
-
     }
 }
