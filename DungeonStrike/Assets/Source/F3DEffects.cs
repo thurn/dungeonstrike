3ef36ca050e1b3524ec5898d054565a7615d2103
Derek Thurn <derek@fake.email>
2016-11-22 23:08:35 -0800

    object pooling
    KEY: 435d09118cd31536f39bf4f0f31168da

--- DungeonStrike/Assets/Source/F3DEffects.cs
+++ DungeonStrike/Assets/Source/F3DEffects.cs
@@ -1,70 +1,57 @@
 ﻿using UnityEngine;
-using Forge3D;
-using System.Collections.Generic;
 
 namespace DungeonStrike
 {
 
     public class F3DEffects : MonoBehaviour
     {
+        public GameObject VulcanImpact;
+        public GameObject VulcanMuzzle;
+        public GameObject VulcanProjectile;
+        public GameObject VulcanShell;
+        public GameObject VulcanSpark;
+        public AudioClip VulcanShotAudio;
+        public AudioClip VulcanImpactAudio;
 
-        private Dictionary<string, Transform> _poolTransforms = new Dictionary<string, Transform>();
-        private F3DPool _pool;
-        private F3DAudioController _f3DAudioController;
-
-        void Start()
+        private void Start()
         {
-            _f3DAudioController = GetComponent<F3DAudioController>();
-            _pool = F3DPoolManager.Pools["GeneratedPool"];
-            foreach (var poolKey in _pool.templates)
-            {
-                _poolTransforms[poolKey.name] = poolKey;
-            }
+            InitializePool(VulcanMuzzle);
+            InitializePool(VulcanProjectile);
         }
 
         public void FireVulcan(Transform parentTransform)
         {
-            var vulcanMuzzleFlash = _poolTransforms["vulcan_muzzle_example"];
-            _pool.Spawn(vulcanMuzzleFlash, parentTransform.position, parentTransform.rotation, parentTransform);
+            CreateFromPrefab(VulcanMuzzle, parentTransform);
+            CreateFromPrefab(VulcanProjectile, parentTransform);
 
-            var vulcanProjectile = _poolTransforms["vulcan_projectile_example"];
-            _pool.Spawn(vulcanProjectile, parentTransform.position, parentTransform.rotation, null /* parent */);
-
-            _f3DAudioController.VulcanShot(parentTransform.position);
-//            // Get random rotation that offset spawned projectile
-//            Quaternion offset = Quaternion.Euler(UnityEngine.Random.onUnitSphere);
-//            // Spawn muzzle flash and projectile with the rotation offset at current socket position
-//            F3DPoolManager.Pools["GeneratedPool"].Spawn(vulcanMuzzle, TurretSocket[curSocket].position,
-//                TurretSocket[curSocket].rotation, TurretSocket[curSocket]);
-//            GameObject newGO =
-//                F3DPoolManager.Pools["GeneratedPool"].Spawn(vulcanProjectile,
-//                    TurretSocket[curSocket].position + TurretSocket[curSocket].forward,
-//                    offset * TurretSocket[curSocket].rotation, null).gameObject;
-//
-//            F3DProjectile proj = newGO.gameObject.GetComponent<F3DProjectile>();
-//            if (proj)
-//            {
-//                proj.SetOffset(vulcanOffset);
-//            }
-//
-//            // Emit one bullet shell
-//            if (ShellParticles.Length > 0)
-//                ShellParticles[curSocket].Emit(1);
-//
-//            // Play shot sound effect
-//            F3DAudioController.instance.VulcanShot(TurretSocket[curSocket].position);
-//
-//            // Advance to next turret socket
-//            AdvanceSocket();
+            var audioSource = parentTransform.GetComponent<AudioSource>();
+            Preconditions.CheckState(audioSource != null, "Firing source must have an AudioSource.");
+            audioSource.clip = VulcanShotAudio;
+            audioSource.pitch = Random.Range(0.95f, 1f);
+            audioSource.volume = Random.Range(0.8f, 1f);
+            audioSource.minDistance = 5f;
+            audioSource.loop = false;
+            audioSource.Play();
         }
 
-        // Spawn vulcan weapon impact
-//        public void VulcanImpact(Vector3 pos)
-//        {
-//            // Spawn impact prefab at specified position
-//            F3DPoolManager.Pools["GeneratedPool"].Spawn(vulcanImpact, pos, Quaternion.identity, null);
-//            // Play impact sound effect
-//            F3DAudioController.instance.VulcanHit(pos);
-//        }
+        private static void InitializePool(GameObject prefab)
+        {
+            var pool = FastPoolManager.GetPool(prefab);
+            pool.Capacity = 10;
+            pool.PreloadCount = 1;
+            pool.NotificationType = PoolItemNotificationType.Interface;
+        }
+
+        private static GameObject CreateFromPrefab(GameObject prefab, Transform parent)
+        {
+            var pool = FastPoolManager.GetPool(prefab, createIfNotExists: false);
+            var result = pool.FastInstantiate(parent);
+            var despawn = result.GetComponent<FPUniversalDespawner>();
+            if (despawn != null)
+            {
+                despawn.TargetPoolID = pool.ID;
+            }
+            return result;
+        }
     }
 }
---
+++ DungeonStrike/Assets/Source/Projectile.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace DungeonStrike
+{
+    public class Projectile : MonoBehaviour
+    {
+        public float Velocity;
+        public LayerMask LayerMask;
+        private ParticleSystem[] _childParticleSystems;
+
+        private void Update()
+        {
+            // Projectile step per frame based on velocity and time
+            var step = transform.forward * Time.deltaTime * Velocity;
+
+            RaycastHit hitPoint;
+            // Raycast for targets with ray length based on frame step by ray cast advance multiplier
+            if (Physics.Raycast(transform.position, transform.forward, out hitPoint, step.magnitude * 2.0f, LayerMask))
+            {
+                FastPoolManager.GetPool(gameObject).FastDestroy(gameObject);
+            }
+
+            // Advances projectile forward
+            transform.position += step;
+        }
+    }
+}
